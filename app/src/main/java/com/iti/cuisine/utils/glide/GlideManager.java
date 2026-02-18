5f9ca8a0440200462914b9d6c251d4b5d4751f9d
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-02-18 12:13:06 +0200

    improved image loading

--- app/src/main/java/com/iti/cuisine/utils/glide/GlideManager.java
+++ app/src/main/java/com/iti/cuisine/utils/glide/GlideManager.java
@@ -16,43 +16,89 @@ import com.bumptech.glide.request.transition.Transition;
 import com.google.android.material.chip.Chip;
 import com.iti.cuisine.R;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class GlideManager {
 
-    public static void loadInto(String url, ImageView imageView) {
-        DrawableCrossFadeFactory factory =
-                new DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build();
+    private static final DrawableCrossFadeFactory factory =
+            new DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build();
 
-        Glide.with(imageView)
+    private static final Queue<Runnable> loadQueue = new LinkedList<>();
+    private static boolean isLoading = false;
+
+    private static void enqueue(Runnable task) {
+        loadQueue.add(task);
+        if (!isLoading) processNext();
+    }
+
+    private static void processNext() {
+        Runnable next = loadQueue.poll();
+        if (next == null) {
+            isLoading = false;
+            return;
+        }
+        isLoading = true;
+        next.run();
+    }
+
+    private static void onLoadFinished() {
+        processNext();
+    }
+
+    public static void loadInto(String url, ImageView imageView) {
+        enqueue(() -> Glide.with(imageView)
                 .load(url)
                 .centerCrop()
                 .transition(DrawableTransitionOptions.withCrossFade(factory))
                 .error(R.drawable.img_error_placeholder)
-                .placeholder(R.drawable.ic_loading_placeholder)
-                .into(imageView);
+                .into(new CustomTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource,
+                                                @Nullable Transition<? super Drawable> transition) {
+                        imageView.setImageDrawable(resource);
+                        onLoadFinished();
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+                        onLoadFinished();
+                    }
+
+                    @Override
+                    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                        imageView.setImageDrawable(errorDrawable);
+                        onLoadFinished();
+                    }
+                }));
     }
 
     public static void loadImageIntoChip(String imageUrl, Chip chip) {
-        chip.setChipIconResource(R.drawable.ic_loading_placeholder);
-
-        Glide.with(chip)
+        enqueue(() -> Glide.with(chip)
                 .asBitmap()
                 .load(imageUrl)
                 .override(48, 48)
                 .into(new CustomTarget<Bitmap>() {
                     @Override
-                    public void onResourceReady(@NonNull Bitmap bitmap, @Nullable Transition<? super Bitmap> transition) {
+                    public void onResourceReady(@NonNull Bitmap bitmap,
+                                                @Nullable Transition<? super Bitmap> transition) {
                         chip.setChipIcon(
                                 new BitmapDrawable(chip.getContext().getResources(), bitmap)
                         );
+                        onLoadFinished();
                     }
 
                     @Override
-                    public void onLoadCleared(@Nullable Drawable placeholder) {}
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+                        onLoadFinished();
+                    }
 
                     @Override
                     public void onLoadFailed(@Nullable Drawable errorDrawable) {
                         chip.setChipIconResource(R.drawable.logo);
+
+                        onLoadFinished();
                     }
-                });
+                }));
     }
 }
